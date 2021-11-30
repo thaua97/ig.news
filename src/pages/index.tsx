6eda7bf474e3c4effad9fa34bdec5be0464cd02a
Thauã Borges <thaua97@gmail.com>
2021-11-29 21:51:35 -0300

    :art: Create homepage styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -6,7 +7,18 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>Hello Wolrd</h1>
+      
+      <main className={styles.content}>
+        <section className={styles.hero}>
+          <span>👏  Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>Get acess to all publications <br />
+            <span> for $9.90 mouth</span>
+          </p>
+          
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }
